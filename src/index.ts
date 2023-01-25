59958af9b689134ce631cadca15917b7fa75adab
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2023-01-25 13:29:05 -0300

    get users mongo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import express from "express";
 import { config } from "dotenv";
+import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
-config();
 
-const app = express();
 
-const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log('listening on port:', port))
+const main = async () => {
+  config();
+  
+  const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world!')
-})
+  await MongoClient.connect();
+
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+  
+    const { body, statusCode } = await getUsersController.handle();
+  
+    res.send(body).status(statusCode);
+  });
+  
+
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => console.log("listening on port:", port));
+};
+
+main()
